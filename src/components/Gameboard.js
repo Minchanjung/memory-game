881fad4e5216554916e170b1df0766b12c801987
Minchan Jung <minchanjung1@gmail.com>
2022-05-23 00:05:43 +0100

    added shuffle functionality

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -17,37 +17,118 @@ import inrainbows from '../imgs/in_rainbows.jpeg';
 
 const Gameboard = (props) => {
     const [cards, setCards] = useState({MBDTF: false, GKMC: false, Ilmatic: false, Faces: false, TMB: false, TPAB: false, Circles: false, Swimming: false, TMOTM: false, Nectar: false, UP: false, inRainbows: false});
+    const [albums, setAlbums] = useState(
+        [{
+            id: "MBDTF",
+            caption: "My Beautiful Dark Twisted Fantasy",
+            src: mbdtf
+        }, 
+        {
+            id: "GKMC",
+            caption: "Good Kid Maad City",
+            src: gkmc
+        },
+         {
+            id: "Ilmatic",
+            caption: "Ilmatic",
+            src: ilmatic
+         }, 
+         {
+            id: "Faces",
+            caption: "Faces",
+            src: faces
+         }, 
+         {
+            id: "TMB",
+            caption: "The Melodic Blue",
+            src: tmb
+         }, 
+         {
+            id: "TPAB",
+            caption: "To Pimp A Butterfly",
+            src: tpab
+         }, 
+         {
+            id: "Circles",
+            caption: "Circles",
+            src: circles
+         }, 
+         {
+            id: "Swimming",
+            caption: "Swimming",
+            src: swimming
+         }, 
+         {
+            id: "TMOTM",
+            caption: "The Man On The Moon",
+            src: tmotm
+         }, 
+         {
+            id: "Nectar",
+            caption: "Nectar",
+            src: nectar
+         }, 
+         {
+            id: "UP",
+            caption: "Under Pressure",
+            src: up
+         }, 
+         {
+            id: "inRainbows",
+            caption: "In Rainbows",
+            src: inrainbows
+         }]
+    );
 
     const handleClick = (e) => {   
         const {id} = e.target; 
-        
+        shuffle(albums);
+
         if (cards[id] === false) {
             setCards(() => ({...cards, [id]: true,}));
-            console.log(cards[id]);
             props.setScore(props.score + 1);
-            console.log(cards)
-            console.log(props.score)
             if (props.score >= 11) {
-                console.log('lastmove');
                 props.endGame();
             }
         }
         if (cards[id] === true) {
-            /*for (const property in cards) {
-                console.log(typeof(property));
-                setCards(() => ({...cards, [property]: false}))
-                console.log(cards[property])
-            }*/
-            console.log(cards[id]);
             setCards(() => ({...cards, [id]: false}))
-            console.log(cards[id])
             props.endGame();
         } 
     }
 
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+        console.log(currentIndex);
+        console.log(randomIndex);
+      
+        // While there remain elements to shuffle.
+        while (currentIndex !== 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          console.log(randomIndex)
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+        console.log(array)
+        return array;
+    }
+
     return (
         <div className='cardContainer'>
-            <Card onClick={handleClick} id='MBDTF' caption='My Beautiful Dark Twisted Fantasy' src={mbdtf}/>
+            {albums.map((album, index) => {
+                return <Card onClick={handleClick} id={album.id} caption={album.caption} src={album.src}/>
+            })}
+        </div>
+    )
+}
+
+export default Gameboard
+/*            <Card onClick={handleClick} id='MBDTF' caption='My Beautiful Dark Twisted Fantasy' src={mbdtf}/>
             <Card onClick={handleClick} id='GKMC' caption='Good Kid Maad City' src={gkmc}/>
             <Card onClick={handleClick} id='Ilmatic' caption='Ilmatic' src={ilmatic}/>
             <Card onClick={handleClick} id='Faces' caption='Faces' src={faces}/>
@@ -58,9 +139,4 @@ const Gameboard = (props) => {
             <Card onClick={handleClick} id='TMOTM' caption='The Man On The Moon' src={tmotm}/>
             <Card onClick={handleClick} id='Nectar' caption='Nectar' src={nectar}/>
             <Card onClick={handleClick} id='UP' caption='Under Pressure' src={up}/>
-            <Card onClick={handleClick} id='inRainbows' caption='In Rainbows' src={inrainbows}/>
-        </div>
-    )
-}
-
-export default Gameboard
+            <Card onClick={handleClick} id='inRainbows' caption='In Rainbows' src={inrainbows}/>*/
